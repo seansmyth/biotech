42c4111f2a6de9a7405f75352d4cb3f08b310c82
Sean Smyth <seansmyth1990@gmail.com>
2020-01-05 15:37:27 +0000

    Added content to Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,118 @@
 // import { Link } from "gatsby"
 // import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
 import React from "react"
 import("./footer.scss")
 
 const Footer = ({}) => (
-  <section className="footer">
-    <h1>footer</h1>
-  </section>
+  <footer className="footer">
+    <ul>
+      <li>About Us</li>
+      <li>
+        <Link to="/">About</Link>
+      </li>
+      <li>
+        <Link to="/">Vision</Link>
+      </li>
+      <li>
+        <Link to="/">Awards</Link>
+      </li>
+      <li>
+        <Link to="/">CSR</Link>
+      </li>
+      <li>
+        <Link to="/">Industries</Link>
+      </li>
+      <li>
+        <Link to="/">Careers</Link>
+      </li>
+      <li>
+        <Link to="/">Contact Us</Link>
+      </li>
+    </ul>
+    <ul>
+      <li>Accreditation</li>
+      <li>
+        <Link to="/">Validated Methods</Link>
+      </li>
+      <li>
+        <Link to="/">ISO</Link>
+      </li>
+      <li>
+        <Link to="/">REACH</Link>
+      </li>
+    </ul>
+    <ul>
+      <li>Customer Support</li>
+      <li>
+        <Link to="/">Ordering</Link>
+      </li>
+      <li>
+        <Link to="/">Payment</Link>
+      </li>
+      <li>
+        <Link to="/">Shipping & Delivery</Link>
+      </li>
+      <li>
+        <Link to="/">Returns</Link>
+      </li>
+      <li>
+        <Link to="/">My Account</Link>
+      </li>
+      <li>
+        <Link to="/">FAQs</Link>
+      </li>
+      <li>
+        <Link to="/">Customer Support</Link>
+      </li>
+      <li>
+        <Link to="/">Ask A Scientist</Link>
+      </li>
+    </ul>
+
+    <ul>
+      <li>Services</li>
+      <li>
+        <Link to="/">Analytical Services</Link>
+      </li>
+      <li>
+        <Link to="/">Consultancy</Link>
+      </li>
+    </ul>
+
+    <ul>
+      <li>News</li>
+      <li>
+        <Link to="/">Press</Link>
+      </li>
+      <li>
+        <Link to="/">News</Link>
+      </li>
+      <li>
+        <Link to="/">Conferencers & Events</Link>
+      </li>
+      <li>
+        <Link to="/">New Products</Link>
+      </li>
+    </ul>
+
+    <ul>
+      <li>Legal</li>
+      <li>
+        <Link to="/">Imprint</Link>
+      </li>
+      <li>
+        <Link to="/">Ts&Cs</Link>
+      </li>
+      <li>
+        <Link to="/">Privacy Policy</Link>
+      </li>
+      <li>
+        <Link to="/">Cookies</Link>
+      </li>
+    </ul>
+  </footer>
 )
 
 export default Footer
--- src/components/home/products.js
+++ src/components/home/products.js
@@ -1,12 +1,17 @@
 // import { Link } from "gatsby"
 // import PropTypes from "prop-types"
 import React from "react"
+// import containerStyles from "./products.module.scss"
 import("./products.scss")
 
 const Products = ({}) => (
   <section className="products">
     <h1>
-      BioTech{" "}
+      Products{" "}
+      <ul>
+        <li>Sure look </li>
+        <div>OMG</div>
+      </ul>
       <span>
         A recognised world leader in the prduction and development of chemical
         agents & chemical kits.
